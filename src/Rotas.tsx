4ab0885c43e130cc5662bee0eb69cb78880e46d3
gevasconcelos <geminivasconceelos@gmail.com>
2022-11-24 16:56:02 -0300

    feat: adicao da rota da dashboard e protecao da rota para a dashboard

--- src/Rotas.tsx
+++ src/Rotas.tsx
@@ -1,31 +1,32 @@
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Navigate,
-    Outlet
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
 } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function ProtectedRoute(props: any) {
+  const token = localStorage.getItem("token");
+  return token ? <Outlet /> : <Navigate to={props.redirect} />;
+}
 
 export default function MyRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='login' element={<Login />} />
-        {/* <Route path='/SignUp' element={<SignUp />} />
-        <Route path='/Login' element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
 
-        <Route element={<ProtectedRoute redirect="/Login" />}>
-        <Route path='/History' element={<History />} />
-        <Route path='/Calculation' element={<Calculation />} />
-        <Route path='/Result' element={<Result />} /> */}
-        {/* </Route> */}
-
-        {/* <Route path='/Help' element={<Help/>} /> */}
-        
+        <Route element={<ProtectedRoute redirect="login" />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
-};
+}
